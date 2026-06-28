62c9e74c7b5bd510cebd3610ea6ad00484c74206
Suryaa11 <ksurya112003@gmail.com>
2026-06-29 00:09:52 +0530

    Updated

--- modules/vnet_peering/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  description = "ID of the virtual network peering."
-  value       = azurerm_virtual_network_peering.this.id
-}
-
-output "name" {
-  description = "Name of the virtual network peering."
-  value       = azurerm_virtual_network_peering.this.name
-}
